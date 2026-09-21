ad210f978f4be8b902db1f2c09c8c91c581fcfa9
Bhavesh Prajapati <pbhavesh45@gmail.com>
2026-09-21 17:32:55 +0530

    updated new version of app

--- src/electron/ipc/handlers/api-handlers.ts
+++ src/electron/ipc/handlers/api-handlers.ts
@@ -17,10 +17,6 @@ import {
   stopRemoteAccessService,
 } from "../../services/remote-access/remoteAccessService.js";
 import { ensureSchedulerInitialized, teardownScheduler } from "../../services/scheduler/bootstrap.js";
-import {
-  restartRemoteAccessService,
-  stopRemoteAccessService,
-} from "../../services/remote-access/remoteAccessService.js";
 
 // Types for IPC events
 export interface ApiConfig {
